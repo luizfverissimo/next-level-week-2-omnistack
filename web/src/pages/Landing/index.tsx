645ea8cfbaa1107479eebb02f0c4ebb556b689c8
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-08-09 11:45:51 -0300

    aula3

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import landingImg from '../../assets/landing.svg';
@@ -9,7 +11,17 @@ import purpleHeartIcon from '../../assets/icons/purple-heart.svg';
 
 import './styles.css'
 
+
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      const { total } = response.data
+      setTotalConnections(total)
+    })
+  }, [])
+
   return (
     <div id='page-landing'>
       <div id='page-landing-content' className='container'>
@@ -33,7 +45,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
 
       </div>
