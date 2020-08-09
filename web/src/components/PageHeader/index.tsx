4a9dc2d569c10b6d7f661e214b85171ccdd0f036
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-08-08 22:05:41 -0300

    Aula3

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,27 +4,33 @@ import { Link } from 'react-router-dom';
 import backIcon from '../../assets/icons/back.svg';
 import logoImg from '../../assets/logo.svg';
 
-import './styles.css'
+import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title, children}) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  children,
+  description,
+}) => {
   return (
     <header className='page-header'>
-        <div className='top-bar-container'>
-          <Link to='/' className=''>
-            <img src={backIcon} alt='Voltar' />
-          </Link>
-          <img src={logoImg} alt='Proffy' />
-        </div>
-        <div className="header-content">
-          <strong>{title}</strong>
-          {children}
-        </div>        
-      </header>
-  )
-}
+      <div className='top-bar-container'>
+        <Link to='/' className=''>
+          <img src={backIcon} alt='Voltar' />
+        </Link>
+        <img src={logoImg} alt='Proffy' />
+      </div>
+      <div className='header-content'>
+        <strong>{title}</strong>
+        {description && <p>{description}</p>}
+        {children}
+      </div>
+    </header>
+  );
+};
 
 export default PageHeader;
